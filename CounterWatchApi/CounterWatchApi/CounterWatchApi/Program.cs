5404382e212907327a5163419795fd91e7238cae
Petro <petrotymchuck@gmail.com>
2026-01-12 16:38:32 +0200

    Add db connection

--- CounterWatchApi/CounterWatchApi/CounterWatchApi/Program.cs
+++ CounterWatchApi/CounterWatchApi/CounterWatchApi/Program.cs
@@ -1,14 +1,17 @@
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
