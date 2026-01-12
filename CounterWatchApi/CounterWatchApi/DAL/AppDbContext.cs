686eaaee460bea1cf36a51ab4fef7b02db57e894
Petro <petrotymchuck@gmail.com>
2026-01-12 16:31:39 +0200

    Add EF Core setup, base entities, and project references

---
+++ CounterWatchApi/CounterWatchApi/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
+    {
+    }
+
+    public DbSet<TestTableEntity> TestTableEntities { get; set; }
+}
---
+++ CounterWatchApi/CounterWatchApi/DAL/Entities/Common/TestTableEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities.Common;
+
+[Table("tbl_test")]
+public class TestTableEntity : BaseEntity<long>
+{
+    public string Text { get; set; } = string.Empty;
+}
