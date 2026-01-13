fd946b2ebaa74c864f99c5af2d2b6b7985860779
Petro <149708697+PiterTimch@users.noreply.github.com>
2026-01-13 13:31:27 +0200

    Start work with Genres (#1)
    * Update appsettings.json with base settings
    
    * Add BLL and DAL projects to solution
    
    * Add EF Core setup, base entities, and project references
    
    * Add db connection
    
    * Add tbl_test table migration and update DbContext setup
    
    * Add Swagger/OpenAPI with JWT auth and static file support
    
    * add genres entity
    
    * Add tbl_genres table via EF Core migration
    
    * Add genre search and pagination models to BLL
    
    * Add genre search service, mapper, and interface
    
    * Add genre creation model and service method stubs
    
    * Add IImageService and ImageService for image handling
    
    * Implement image handling in GenreService and DI setup
    
    * Add Genres API, remove WeatherForecast, add image assets

---
+++ CounterWatchApi/CounterWatchApi/BLL/Mappers/GenreMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BLL.Models.Genre;
+using DAL.Entities.Genre;
+
+namespace BLL.Mappers;
+
+public class GenreMapper : Profile
+{
+    public GenreMapper()
+    {
+        CreateMap<GenreEntity, GenreItemModel>();
+
+        CreateMap<GenreCreateModel, GenreEntity>()
+            .ForMember(x => x.Image, opt => opt.Ignore());
+    }
+}
---
+++ CounterWatchApi/CounterWatchApi/DAL/Entities/Genre/GenreEntity.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAL.Entities.Genre;
+
+[Table("tbl_genres")]
+public class GenreEntity : BaseEntity<long>
+{
+    [StringLength(255)]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(255)]
+    public string? Image { get; set; }
+}
+
