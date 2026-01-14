5d226fb05c5efc4739413ff56e94c432b8d39ed7
Petro <petrotymchuck@gmail.com>
2026-01-14 10:40:58 +0200

    working dbseed

--- CounterWatchApi/CounterWatchApi/BLL/Services/DbSeeder.cs
+++ CounterWatchApi/CounterWatchApi/BLL/Services/DbSeeder.cs
@@ -1,11 +1,25 @@
 ﻿using BLL.Interfaces;
+using DAL;
+using DAL.Entities.Genre;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace BLL.Services;
 
-public class DbSeeder(IGenresService genresService) : IDbSeeder
+public class DbSeeder(IServiceProvider serviceProvider) : IDbSeeder
 {
     public async Task SeedData()
     {
-        await genresService.SeedGenresAsync(Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "JsonData", "Genres.json"));
+        using var scope = serviceProvider.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var genresService = scope.ServiceProvider.GetRequiredService<IGenresService>();
+
+        context.Database.Migrate();
+
+        await genresService.SeedGenresAsync(
+            Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "JsonData", "Genres.json")
+        );
     }
 }
