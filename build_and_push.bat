712265e06d656f0a14789b6355dab71994bd8df7
Petro <petrotymchuck@gmail.com>
2026-01-15 11:32:01 +0200

    working docker

--- build_and_push.bat
+++ build_and_push.bat
@@ -7,7 +7,7 @@ docker tag counterwatch-web:latest pedro007salo/counterwatch-web:latest
 docker push pedro007salo/counterwatch-web:latest
 
 REM ==== API ====
-cd ..\..\CounterWatchApi
+cd ..\..\CounterWatchApi\CounterWatchApi
 docker build -t counterwatch-api .
 docker tag counterwatch-api:latest pedro007salo/counterwatch-api:latest
 docker push pedro007salo/counterwatch-api:latest
