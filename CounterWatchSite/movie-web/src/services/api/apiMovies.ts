70237ff9ce97514d0e26e741fb7288f06a587377
Petro <petrotymchuck@gmail.com>
2026-01-18 11:35:38 +0200

    base home page work

--- CounterWatchSite/movie-web/src/services/api/apiMovies.ts
+++ CounterWatchSite/movie-web/src/services/api/apiMovies.ts
@@ -1,12 +1,12 @@
 import {createApi} from "@reduxjs/toolkit/query/react";
 import {createBaseQuery} from "../../utils/createBaseQuery.ts";
 import type {IPagedResult} from "../../types/aditional.ts";
-import type {IMovieItem, IMovieSearch} from "../../types/movie.ts";
+import type {IMovieGetBySlug, IMovieItem, IMovieSearch} from "../../types/movie.ts";
 
 export const apiMovies = createApi({
     reducerPath: 'api/movies',
     baseQuery: createBaseQuery('Movies'),
-    tagTypes: ['Movies'],
+    tagTypes: ['Movies', "Movie"],
     endpoints: (builder) => ({
         searchMovies: builder.query<IPagedResult<IMovieItem>, IMovieSearch>({
             query: () => ({
@@ -15,9 +15,17 @@ export const apiMovies = createApi({
             }),
             providesTags: ['Movies']
         }),
+
+        getBySlug: builder.query<IMovieItem, IMovieGetBySlug>({
+            query: (slug) => ({
+                url: `BySlug/${slug}`,
+                method: 'GET'
+            }),
+        })
     })
 });
 
 export const {
     useSearchMoviesQuery,
+    useGetBySlugQuery,
 } = apiMovies;
